409df2330bef53a8207ea1c2cad2a36d1a1a44fb
= <mukul02015@gmail.com>
2020-11-23 12:25:52 +1100

    setup separate location and ship pages

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const app = express();
 
-const shipRoutes = require('./ships');
-app.use('/ships', shipRoutes);
+const ships = require('./ships');
+const locations = require('./locations');
 
-
-module.exports = app;
+app.use('/ships', ships);
+app.use('/locations', locations);
 
 app.get('/', (req, res) => {
     res.send('Stomble RESTful API!');
 });
-
-app.get('/api/ships', (req, res) => {
-    res.send([1,2,3]);
-});
-
-app.get('/ships', app.list);
-
 
 app.listen(3000, () => console.log('Listening on port 3000..'));
--- locations.js
+++ locations.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 // JSON objects storing ships
-var ships = [
-    {id: '1', name: 'A', model: 'AA', location: 'AL', status: 'D'},
-    {id: '2', name: 'B', model: 'BB', location: 'BL', status: 'M'}
+var locations = [
+    {id: '1', cname: 'cloudy', pname: 'alienation', capacity: 9},
+    {id: '2', cname: 'foggy', pname: 'ghostnation', capacity: 10}
 ];
 
 // Ships home page route
 router.get('/', (req, res) => {
-    res.send('Ships page');
+    res.send('Locations page');
 })
 
 // Ships list route
 router.get('/list', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(ships, null, 3));
+    res.end(JSON.stringify(locations, null, 3));
 })
 
 module.exports = router;
