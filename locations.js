41d6e0eed879910f8fdd8ee34274dd2cea429818
= <mukul02015@gmail.com>
2020-11-24 12:10:02 +1100

    add post requests to ship and locations

--- locations.js
+++ locations.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-// JSON objects storing ships
+// handling json post requests
+router.use(express.json())
+
+
+// JSON objects storing locations
 var locations = [
-    {id: '1', cname: 'cloudy', pname: 'alienation', capacity: 9},
-    {id: '2', cname: 'foggy', pname: 'ghostnation', capacity: 10}
+    {id: 1, cname: 'cloudy', pname: 'alienation', capacity: 9},
+    {id: 2, cname: 'foggy', pname: 'ghostnation', capacity: 10}
 ];
 
-// Ships home page route
+// location root page
 router.get('/', (req, res) => {
     res.send('Locations page');
-})
+});
 
-// Ships list route
-router.get('/list', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(locations, null, 3));
-})
+// get location by id
+router.get('/:id', (req, res) => {
+    if (req.params.id === 'list'){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(locations, null, 3));
+    }
+    const loc = locations.find(c => c.id === parseInt(req.params.id));
+    if (!loc) res.status(404).send('The location with given id does not exist');
+    res.end(JSON.stringify(loc, null, 3));
+});
+
+// post location to locations list
+router.post('/', (req, res) => {
+    const loc = {
+        id: locations.length+1,
+        cname: req.body.cname,
+        pname: req.body.pname,
+        capacity: req.body.capacity
+    };
+    locations.push(loc);
+    res.send(loc);
+});
 
 module.exports = router;
--- ships.js
+++ ships.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
+// handling json post requests
+router.use(express.json());
+
+
 // JSON objects storing ships
 var ships = [
-    {id: '1', name: 'A', model: 'AA', location: 'AL', status: 'D'},
-    {id: '2', name: 'B', model: 'BB', location: 'BL', status: 'M'}
+    {id: 1, name: 'A', model: 'AA', location: ['cloudy', 'alienation'], status: 'D'},
+    {id: 2, name: 'B', model: 'BB', location: ['foggy', 'ghostnation'], status: 'M'}
 ];
 
-// Ships home page route
+// get ships root page
 router.get('/', (req, res) => {
     res.send('Ships page');
 })
 
-// Ships list route
+// get ship by id
+router.get('/:id', (req, res) => {
+    // to list all ships
+    if (req.params.id === 'list'){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(ships, null, 3));
+    }
+    const ship = ships.find(c => c.id === parseInt(req.params.id));
+    if(!ships) res.status(404).send('Ship with the given id does not exist');
+    res.end(JSON.stringify(ship, null, 3));
+});
+
+// get Ships list
 router.get('/list', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(ships, null, 3));
 })
+
+// post ship to ships list
+router.post('/', (req, res) => {
+    const ship = {
+        id: ships.length+1,
+        name: req.body.name,
+        model: req.body.model,
+        location: req.body.location,
+        status: req.body.status
+    };
+    ships.push(ship);
+    res.send(ship);
+});
 
 module.exports = router;
