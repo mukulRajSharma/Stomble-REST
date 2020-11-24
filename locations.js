8e0dcb0013d30af86be101d0ce3ed2c8ae96577e
= <mukul02015@gmail.com>
2020-11-24 23:46:42 +1100

    store locations in json file

--- locations.js
+++ locations.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+
 const router = express.Router();
 
 // handling json post requests
 router.use(express.json())
 
-
-// JSON objects storing locations
-var locations = [
-    {id: 1, cname: 'cloudy', pname: 'alienation', capacity: 9},
-    {id: 2, cname: 'foggy', pname: 'ghostnation', capacity: 10}
-];
-
+const checkout = typeof window !== 'undefined' ? localStorage.setItem("locationStorage", JSON.stringify(locations)) : null
+//localStorage.setItem("locationStorage", JSON.stringify(locations));
 // location root page
 router.get('/', (req, res) => {
     res.send('Locations page');
@@ -18,11 +15,20 @@ router.get('/', (req, res) => {
 
 // get location by id
 router.get('/:id', (req, res) => {
+
+    // read the locations file
+    let rawLoc = fs.readFileSync('locations.json');
+    let jLoc = JSON.parse(rawLoc);
+    
+    // listing all locations
     if (req.params.id === 'list'){
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(locations, null, 3));
+        res.end(JSON.stringify(jLoc, null, 3));
+        return;
     }
-    const loc = locations.find(c => c.id === parseInt(req.params.id));
+    
+    // listing location by ids
+    const loc = jLoc.find(c => c.id === parseInt(req.params.id));
     if (!loc) {
         res.status(404).send('The location with given id does not exist');
         return;
@@ -38,26 +44,42 @@ router.post('/', (req, res) => {
         res.status(400).send('cname, pname, capacity required !');
         return;
     }
-    const loc = {
-        id: locations.length+1,
+    // load the locations file
+    let rawLoc = fs.readFileSync('locations.json');
+    let jLoc = JSON.parse(rawLoc);
+
+    // make JSON object to be pushed to file
+    let loc = {
+        id: jLoc.length+1,
         cname: req.body.cname,
         pname: req.body.pname,
         capacity: req.body.capacity
     };
-    locations.push(loc);
+
+    // write the changes back to the file
+    jLoc.push(loc);
+    fs.writeFileSync('locations.json', JSON.stringify(jLoc));
     res.send(loc);
 });
 
 // delete location with given id
 router.delete('/:id', (req, res) => {
-    const loc = locations.find(c => c.id === parseInt(req.params.id));
+    // load the locations file
+    let rawLoc = fs.readFileSync('locations.json');
+    let jLoc = JSON.parse(rawLoc);
+
+    // find the location with given id
+    const loc = jLoc.find(c => c.id === parseInt(req.params.id));
     if (!loc) {
         res.status(404).send('Location with given id does not exist');
         return;
     }
-    const index = locations.indexOf(loc);
-    locations.splice(index, 1);
+    // remove element from the JSON locations list
+    const index = jLoc.indexOf(loc);
+    jLoc.splice(index, 1);
 
+    // write the changes back to the file
+    fs.writeFileSync('locations.json', JSON.stringify(jLoc));
     res.send(loc);
 });
 
