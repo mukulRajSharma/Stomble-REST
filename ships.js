5f0e46f2a105c7ee0705548e48e97baeb9a16c14
= <mukul02015@gmail.com>
2020-11-27 22:48:31 +1100

    travel func implemented

--- ships.js
+++ ships.js
@@ -23,25 +23,42 @@ router.put('/travel/:id', (req, res) => {
     let jShip = JSON.parse(rawShip);
     
     // check status of the ship
-    const ship = jShip.find(c => c.id === req.params.id);
+    const ship = jShip.find(c => c.id === parseInt(req.params.id));
     if (!ship) return res.status(400).send('Ship with given id does not exist!');
-    if (ship.id != "O") return res.status(400).send('Ship with given id is not operational!');
+    if (ship.status != "O") return res.status(400).send('Ship with given id is not operational!');
 
     // read the locations file
     let rawLoc = fs.readFileSync('./data/locations.json');
     let jLoc = JSON.parse(rawLoc);
 
     // find location with requested city and planet
-    const loc = jLoc.find(c => c.cname === req.params.cname && c.pname === req.params.pname);
+    const loc = jLoc.find(c => c.cname === req.body.cname && c.pname === req.body.pname);
     // location not found error
     if (!loc) return res.status(400).send('Location with given name does not exist!');
+
+    // destination same as origin
+    if (req.body.cname === ship.location[0] && req.body.pname === ship.location[1]) return res.status(400).send('Present location is the destination!');
 
     // location found, check destination capacity
     if (loc.capacity < 1) return res.status(400).send('Location has reached max capacity!');
 
-    // location found and has available space
-    ship.find
+    // update the capacity of the old location
+    const loc_old = jLoc.find(c => c.cname === ship.location[0] && c.pname === ship.location[1]);
+    loc_old.capacity = loc_old.capacity + 1;
 
+    // change location
+    ship.location[0] = req.body.cname;
+    ship.location[1] = req.body.pname;
+
+    // decrease location capacity
+    loc.capacity = loc.capacity-1;
+
+    // write the changes back to the data files
+    fs.writeFileSync('./data/ships.json', JSON.stringify(jShip));
+    fs.writeFileSync('./data/locations.json', JSON.stringify(jLoc));
+    
+    // send modified ship object as response    
+    res.end(ship);
 
 });
 
@@ -89,12 +106,19 @@ router.post('/', (req, res) => {
     const checkPlanet = jLoc.find(c => c.pname === req.body.location[1]);
     if (!checkPlanet) return res.status(400).send('Planet name does not exist in locations list!');
 
+    // location found, check destination capacity
+    const loc = jLoc.find(c => c.cname === req.body.location[0] && c.pname === req.body.location[1]);
+    if (loc.capacity < 1) return res.status(400).send('Location has reached max capacity!');
+
     // validate status 
     validStatus = ["D", "O", "M"];
     if(!validStatus.includes(req.body.status)) {
         res.status(400).send('Invalid status value!');
         return;
     }
+    
+    // decrease location capacity count
+    loc.capacity = loc.capacity - 1;
 
     const ship = {
         id: jShip.length+1,
@@ -107,6 +131,7 @@ router.post('/', (req, res) => {
     // write changes to the ships file
     jShip.push(ship);
     fs.writeFileSync('./data/ships.json', JSON.stringify(jShip));
+    fs.writeFileSync('./data/locations.json', JSON.stringify(jLoc));
 
     res.send(ship);
 });
